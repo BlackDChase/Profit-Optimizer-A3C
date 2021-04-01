35793560031dfff539aad68c495561be88de8465
BlackDChase <blackdragonchase@gmail.com>
2021-04-02 03:41:08 +0530

    Logging method cheanged, First run result posted in logs

--- modules/extractLog.sh
+++ modules/extractLog.sh
@@ -4,10 +4,15 @@ mkdir ../Saved_model/$name
 cd "../Saved_model/$name"
 mv "../logs/$name.tsv" "./"
 awk "/\trewards/ {print}" $name.tsv > "rewardLog.tsv"
-awk "/loss/ {print}" $name.tsv > "lossLog.tsv"
+awk "/policy loss/ {print}" $name.tsv > "policyLossLog.tsv"
+awk "/critic loss/ {print}" $name.tsv > "criticLossLog.tsv"
+awk "/Advantage/ {print}" $name.tsv > "advantageLog.tsv"
+
 mv "../*.pt" "./"
 vim rewardLog.tsv  -c ':%s/.*rewards = //g' -c ':%s/\[//g'  -c ':%s/\]//g' -c ':wq'
-vim lossLog.tsv  -c ':%s/.*loss = //g' -c ':wq'
+vim policyLossLog.tsv  -c ':%s/.*loss = //g' -c ':wq'
+vim criticLossLog.tsv  -c ':%s/.*loss = //g' -c ':wq'
+vim advantageLog.tsv  -c ':%s/.*Advantage = //g' -c ':%s/\[//g'  -c ':%s/\]//g' -c ':wq'
 echo "'Post Processing Logs was a success'"
 python ../modules/postProcessing.py $name
 echo "'Ploting was success'"
