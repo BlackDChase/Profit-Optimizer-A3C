7000897d7bbb4aa58a1e16ccfc6e624989479b01
BlackDChase <blackdragonchase@gmail.com>
2021-04-04 02:12:56 +0530

    Fixed #9 [X] Syntax [X] OOM+DataFlow [X] Multiprocessing [ ] Working of Algorithm [ ] GPU Acceleration

--- modules/extractLog.sh
+++ modules/extractLog.sh
@@ -2,10 +2,10 @@
 name=$(date +%a-%d-%m-%y)
 mkdir ../Saved_model/$name
 cd "../Saved_model/$name"
-mv "../logs/$name.tsv" "./"
+mv "../../logs/$name.tsv" "./"
 awk "/\trewards/ {print}" $name.tsv > "rewardLog.tsv"
-awk "/policy loss/ {print}" $name.tsv > "policyLossLog.tsv"
-awk "/critic loss/ {print}" $name.tsv > "criticLossLog.tsv"
+awk "/Policy loss/ {print}" $name.tsv > "policyLossLog.tsv"
+awk "/Critic loss/ {print}" $name.tsv > "criticLossLog.tsv"
 awk "/Advantage/ {print}" $name.tsv > "advantageLog.tsv"
 
 mv "../*.pt" "./"
@@ -14,5 +14,5 @@ vim policyLossLog.tsv  -c ':%s/.*loss = //g' -c ':wq'
 vim criticLossLog.tsv  -c ':%s/.*loss = //g' -c ':wq'
 vim advantageLog.tsv  -c ':%s/.*Advantage = //g' -c ':%s/\[//g'  -c ':%s/\]//g' -c ':wq'
 echo "'Post Processing Logs was a success'"
-python ../modules/postProcessing.py $name
+#python ../modules/postProcessing.py $name
 echo "'Ploting was success'"
