11b1999f5787b472a10177cf8a0545b7f6054137
BlackDChase <blackdragonchase@gmail.com>
2021-04-04 23:08:24 +0530

    Fixed #11 Loaded model usecase not made, Fixed #13 Boss has multiple environment

--- modules/main.py
+++ modules/main.py
@@ -24,7 +24,9 @@ if __name__=="__main__":
         "e":100,
         "t":5,
         "a":3,
-        "d":False
+        "d":False,
+        "alr":1e-3,
+        "clr":1e-3,
     }
     stateSize = 9
     log.info(f"stateSize = {stateSize}")
@@ -33,32 +35,56 @@ if __name__=="__main__":
         key,value = arg.split("=")
         if key[1:]=="d":
             keywords[key[1:]] = True if "t" in value.lower() else False
+            # For debug
+        elif key[1:]=="p":
+            keywords[key[1:]] = value
+            # For path (saved model)
         else:
             keywords[key[1:]] = int(value)
         log.info(f"Parameter {keywords[key[1:]]} is {value}")
-
-    god = GOD(
-        maxEpisode=keywords["e"],
-        nAgent=keywords["n"],
-        debug=keywords["d"],
-        trajectoryLength=keywords["t"],
-        stateSize=stateSize,
-        actionSpaceDeviation=keywords["a"],
-    )
-    log.info("GOD inititated")
-    actionSpace = god.getActionSpace()
-    log.info(f"Action space: {actionSpace}")
-    env = ENV(stateSize,actionSpace)
-    log.info("Environment inititated")
-    god.giveEnvironment(env)
-    log.info("Environment parsed, Boss inititated")
-    threadCount=0
-    try:
-        god.train()
-        god.saveModel("../Saved_model/")
-    except Exception as catch:
-        #log.debug(f"Terminaion Trace back {catch.with_traceback()}")
-        threadCount+=1
-        log.info(f"Terminated on a {threadCount}\t{catch}")
-        log.info(f"Traceback for the {threadCount} Exception\t{sys.exc_info()}")
-        print(f"{threadCount} thread Terminated, check log {log.name}")
+    if "h" in keywords:
+    if "p" not in keywords.keys():
+        god = GOD(
+            stateSize=stateSize,
+            actionSpaceDeviation=keywords["a"],
+            debug=keywords["d"],
+            maxEpisode=keywords["e"],
+            nAgent=keywords["n"],
+            trajectoryLength=keywords["t"],
+            alr=keywords["alr"],
+            clr=keywords["clr"]
+        )
+        log.info("GOD inititated")
+        actionSpace = god.getActionSpace()
+        log.info(f"Action space: {actionSpace}")
+        env = ENV(stateSize,actionSpace)
+        log.info("Environment inititated")
+        god.giveEnvironment(env)
+        log.info("Environment parsed, Boss inititated")
+        threadCount=0
+        try:
+            god.train()
+            god.saveModel("../Saved_model/")
+        except Exception as catch:
+            #log.debug(f"Terminaion Trace back {catch.with_traceback()}")
+            threadCount+=1
+            log.info(f"Terminated on a {threadCount}\t{catch}")
+            log.info(f"Traceback for the {threadCount} Exception\t{sys.exc_info()}")
+            print(f"{threadCount} thread Terminated, check log {log.name}")
+    else:
+        god = GOD(
+            debug=keywords["d"],
+            stateSize=stateSize,
+            actionSpaceDeviation=keywords["a"],
+            path=keywords["p"],
+        )
+        log.info("GOD inititated")
+        actionSpace = god.getActionSpace()
+        log.info(f"Action space: {actionSpace}")
+        env = ENV(stateSize,actionSpace)
+        log.info("Environment inititated")
+        god.giveEnvironment(env)
+        log.info("Environment parsed, Boss inititated")
+        """
+        Live model use, not implimented
+        #"""
