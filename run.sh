a11d0b1df4433d85c1419c5b174c51188b7c51e2
BlackDChase <blackdragonchase@gmail.com>
2021-04-24 21:14:15 +0530

    Hyperparameters changed

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 # Author  : BlackDChase
 # Version : 0.2.0
 cd modules
-n=5
-e=15
-t=10
-a=5
+n=10
+e=1000
+t=50
+a=7
 alr=0.01
 clr=0.01
-d=True
+#d=True
 echo "'Model will start training with $n agents, training $e episodes of $t length, with $a actions, and Debugging set to $d, while actor learning rate and critic learining rate being at $alr and $clr respectivly, as Hyperparameters'"
 python main.py -n=$n -e=$e -t=$t -a=$a -alr=$alr -clr=$clr -d=$d
 #python main.py -a=7 -d=t -p="../Saved_model/Sun-04-04-21/"
